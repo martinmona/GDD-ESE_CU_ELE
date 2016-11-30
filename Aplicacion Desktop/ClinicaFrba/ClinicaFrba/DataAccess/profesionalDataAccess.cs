0c8dd952be6e525f3bfacc2ca3305dbecbf8cae1
matihuber <matiashuber94@gmail.com>
2016-11-30 12:08:27 -0300

    Turnos Arrancado nuevamente

--- Aplicacion Desktop/ClinicaFrba/ClinicaFrba/DataAccess/profesionalDataAccess.cs
+++ Aplicacion Desktop/ClinicaFrba/ClinicaFrba/DataAccess/profesionalDataAccess.cs
@@ -44,5 +44,32 @@ namespace ClinicaFrba.DataAccess
             conn.Close();
             return listaProfesionals;
         }
+
+        public static List<Profesional> ObtenerProfesionalesXEspecialidad(decimal codigoEspecialidad)
+        {
+            List<Profesional> listaProfesionales = new List<Profesional>();
+            SqlConnection conn = conectar();
+            SqlCommand MiComando = new SqlCommand();
+            MiComando.Connection = conn;
+            MiComando.CommandText = " SELECT CONCAT(pers_nombre, ' ',pers_apellido) as nombre, CONCAT(pers_tipo_documento, ' ',pers_numero_documento) as documento,pers_direccion, pers_telefono, pers_mail, pers_fecha_nacimiento, pers_sexo,  prof_codigo_matricula, prof_codigo_persona FROM ESE_CU_ELE.Profesional, ESE_CU_ELE.Persona, ESE_CU_ELE.EspecialidadXProfesional  WHERE pers_codigo = prof_codigo_persona  AND prof_codigo_persona = espexp_codigo_profesional and espexp_codigo_especialidad = " + codigoEspecialidad;
+            SqlDataReader reader = MiComando.ExecuteReader();
+            while (reader.Read())
+            {
+                Profesional Profesional = new Profesional();
+                Profesional.codigoPersona = (decimal)reader["prof_codigo_persona"];
+                Profesional.matricula = (decimal)reader["prof_codigo_matricula"];
+                Profesional.nombre = (string)reader["nombre"];
+                Profesional.documento = (string)reader["documento"];
+                Profesional.direccion = (string)reader["pers_direccion"];
+                Profesional.telefono = (decimal)reader["pers_telefono"];
+                Profesional.mail = (string)reader["pers_mail"];
+                Profesional.fechaNacimiento = (DateTime)reader["pers_fecha_nacimiento"];
+                Profesional.sexo = (string)reader["pers_sexo"];
+                listaProfesionales.Add(Profesional);
+            }
+            reader.Close();
+            conn.Close();
+            return listaProfesionales;
+        }
     }
 }
--- Aplicacion Desktop/ClinicaFrba/ClinicaFrba/Pedir Turno/Form1.cs
+++ Aplicacion Desktop/ClinicaFrba/ClinicaFrba/Pedir Turno/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicaFrba.Class;
+using ClinicaFrba.DataAccess;
 
 namespace ClinicaFrba.Pedir_Turno
 {
@@ -19,6 +21,35 @@ namespace ClinicaFrba.Pedir_Turno
 
 
         private void Turno_Load(object sender, EventArgs e)
+        {
+            List<Especialidad> especialidades = especialidadDataAccess.ObtenerEspecialidades("");
+            cmbEspecialidad.DataSource = null;
+            cmbEspecialidad.Items.Clear();
+
+            cmbEspecialidad.DataSource = especialidades;
+            cmbEspecialidad.ValueMember = "espe_codigo";
+            cmbEspecialidad.DisplayMember = "espe_descripcion";
+           
+        }
+
+        private void cmbEspecialidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Especialidad espe = (Especialidad)cmbEspecialidad.SelectedItem;
+            List<Profesional> profesionales = profesionalDataAccess.ObtenerProfesionalesXEspecialidad(espe.codigo);
+            cmbProfesional.DataSource = null;
+            cmbProfesional.Items.Clear();
+
+            cmbProfesional.DataSource = profesionales;
+            cmbProfesional.ValueMember = "prof_codigo_persona";
+            cmbProfesional.DisplayMember = "nombre";
+        }
+
+        private void cmbProfesional_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Calendario_DateChanged(object sender, DateRangeEventArgs e)
         {
 
         }
